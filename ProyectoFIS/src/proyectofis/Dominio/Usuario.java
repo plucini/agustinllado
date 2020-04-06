75f8f77f50795950bd2f294d68c9c46723e5fcf6
Agustin Llado <lladoagustin@gmail.com>
2020-04-05 22:03:15 -0300

    Se creo el proyecto java y la clase usuario en el dominio con sus correspondientes atributos atributos

---
+++ ProyectoFIS/src/proyectofis/Dominio/Usuario.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectofis.Dominio;
+
+/**
+ *
+ * @author agustinllado
+ */
+public class Usuario {
+    
+    private String nombre;
+    private String cedula;
+    private int contraseña;
+    private int edad;
+    private int cantidadDeHijos;
+    private int saldo;
+
+ 
+    
+}
